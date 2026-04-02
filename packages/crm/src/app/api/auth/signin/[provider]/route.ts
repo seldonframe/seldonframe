5551abe91ec58173c3548ce61ce6be5644fb5348
Maxime Houle <maximehoule100@gmail.com>
2026-04-02 07:34:03 -0400

    feat: soul 1000x schema + OAuth simplification

--- packages/crm/src/app/api/auth/signin/[provider]/route.ts
+++ packages/crm/src/app/api/auth/signin/[provider]/route.ts
@@ -1,9 +1,3 @@
 import { handlers } from "@/auth";
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-  const url = new URL("/signup", request.url);
-  return NextResponse.redirect(url);
-}
-
+export const GET = handlers.GET;
 export const POST = handlers.POST;
